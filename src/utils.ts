cdab725f1b69fd67f39d723bc666b039fcb61cd4
FDonovan12 <donovanferreira12@gmail.com>
2025-06-20 01:58:42 +0200

    add resolveSelector to utils

--- src/utils.ts
+++ src/utils.ts
@@ -9,3 +9,38 @@ export const defineIfNotExists = <T extends object>(prototype: T, name: string, 
         });
     }
 };
+
+export function resolveSelector<T, R>(
+    selector?: keyof T | ((item: T) => R),
+    fallback?: (item: T, index?: number) => R,
+    name = 'selector',
+): (item: T, index?: number) => R {
+    assertValidSelector<T, R>(selector);
+
+    if (typeof selector === 'function') {
+        return selector;
+    }
+
+    if (typeof selector === 'string') {
+        return (item: T) => item[selector] as unknown as R;
+    }
+
+    if (selector) {
+        throw new TypeError(`${name} must be a function or a string key`);
+    }
+
+    if (fallback) {
+        return fallback;
+    }
+    throw new TypeError(`${name} must be a function or a string key`);
+}
+
+export function assertValidSelector<T, R>(
+    selector: any,
+    name: string = 'selector',
+): asserts selector is keyof T | ((item: T) => R) {
+    const isValid = typeof selector === 'function' || typeof selector === 'string';
+    if (selector !== undefined && !isValid) {
+        throw new TypeError(`${name} must be a function or a string key`);
+    }
+}
