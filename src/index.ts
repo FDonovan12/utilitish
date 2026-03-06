70942f447e0f2dc4db4c5037256c997a6299c5a1
FDonovan12 <donovanferreira@gmail.com>
2026-03-06 14:26:36 +0100

    import correctly map and set

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import './array/array-constructor';
 import './array/array-prototype';
 
+import './map/map-prototype';
 import './object/object-prototype';
+import './set/set-prototype';
 import './string/string-prototype';
